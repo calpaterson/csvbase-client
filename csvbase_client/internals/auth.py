2f485bf12e9b859883e4abec89f03ba9a0d9de2d
Cal Paterson <cal@calpaterson.com>
2023-08-30 10:22:25 +0100

    Make the auth more robust against netrc issues

--- csvbase_client/internals/auth.py
+++ csvbase_client/internals/auth.py
@@ -1,6 +1,6 @@
 from logging import getLogger
 from typing import Optional
-from netrc import netrc
+import netrc
 
 from .value_objs import Auth
 
@@ -8,7 +8,14 @@ logger = getLogger(__name__)
 
 
 def _get_auth(host) -> Optional[Auth]:
-    netrc_f = netrc()
+    try:
+        netrc_f = netrc.netrc()
+    except FileNotFoundError:
+        logger.info("netrc is absent")
+        return None
+    except netrc.NetrcParseError as e:
+        logger.warning("unable to raise netrc file: %s", e)
+        return None
     netrc_triple = netrc_f.authenticators(host)
     if netrc_triple is None:
         logger.info("nothing found in netrc for host: %s", host)
---
+++ tests/cli/test_auth.py
@@ -0,0 +1,41 @@
+import os
+from unittest.mock import patch
+
+import pytest
+
+from csvbase_client.internals.auth import get_auth
+
+
+def test_netrc(tmpdir):
+    sample_netrc = """
+machine csvbase.com
+  login test
+  password password
+"""
+    netrc = tmpdir / ".netrc"
+    netrc.write(sample_netrc)
+    netrc.chmod(0o600)
+    with patch.dict(os.environ, {"HOME": str(tmpdir)}):
+        auth = get_auth()
+    assert auth.username == "test"
+    assert auth.api_key == "password"
+
+
+def test_netrc_absent(tmpdir):
+    with patch.dict(os.environ, {"HOME": str(tmpdir)}):
+        auth = get_auth()
+    assert auth is None
+
+
+def test_netrc_too_permissive(tmpdir):
+    netrc = tmpdir / ".netrc"
+    sample_netrc = """
+machine csvbase.com
+  login test
+  password password
+"""
+    netrc = tmpdir / ".netrc"
+    netrc.write(sample_netrc)
+    with patch.dict(os.environ, {"HOME": str(tmpdir)}):
+        auth = get_auth()
+    assert auth is None
