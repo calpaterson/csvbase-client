90b86d062c37db06b5275a5f0073c0fadce81fd9
Cal Paterson <cal@calpaterson.com>
2023-08-30 13:29:43 +0100

    Improve the setup.py in preparation for pypi submission

--- setup.py
+++ setup.py
@@ -22,11 +22,21 @@ test_reqs = [
     "pandas-stubs",
 ]
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="csvbase-client",
     version=VERSION,
+    author="Cal Paterson",
+    author_email="cal@calpaterson.com",
+    description="The command line client for csvbase",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests.*", "tests"]),
     include_package_data=True,
+    url="https://github.com/calpaterson/csvbase-client",
+    keywords="csv data processing",
     package_data={"csvbase_client": ["VERSION"]},
     install_requires=[
         "click",
@@ -36,6 +46,22 @@ setup(
         "toml",
         "importlib_resources; python_version<'3.9'"
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3 (AGPL-3.0)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    "Topic :: Utilities",
+    ],
+    project_urls={
+        "Bug Tracker": "https://github.com/calpaterson/csvbase-client/issues",
+        "Documentation": "https://github.com/calpaterson/csvbase-client/wiki",
+        "Source Code": "https://github.com/calpaterson/csvbase-client",
+    },
     extras_require={"tests": test_reqs},
     entry_points={
         "console_scripts": [
