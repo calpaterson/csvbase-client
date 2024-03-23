17af2ca9f8587c5ae8d7915d54c4ee132b195dd3
Cal Paterson <cal@calpaterson.com>
2024-03-23 16:52:09 +0000

    Move the test deps into a requirement.txt
    Necessary in order to pull csvbase from git

--- setup.py
+++ setup.py
@@ -6,22 +6,6 @@ VERSION = (
     (pathlib.Path(__file__).parent / "csvbase_client" / "VERSION").open().read().strip()
 )
 
-# The tests test the blog, so it must be installed
-test_reqs = [
-    "black",
-    "bpython",
-    "mypy",
-    "pytest",
-    "ruff",
-    "types-requests",
-    "types-setuptools",
-    "types-toml",
-    "pandas",
-    "pyarrow",
-    "types-passlib",
-    "pandas-stubs",
-]
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
@@ -69,7 +53,6 @@ setup(
         "Source Code": "https://github.com/calpaterson/csvbase-client",
         "Changelog": "https://github.com/calpaterson/csvbase-client/blob/main/CHANGELOG.md",
     },
-    extras_require={"tests": test_reqs},
     entry_points={
         "console_scripts": [
             "csvbase-client=csvbase_client.internals.cli:cli",
