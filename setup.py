4b82d8ed1503ddd284cd8e2b492dcd309b9e922c
Cal Paterson <cal@calpaterson.com>
2024-03-23 16:52:43 +0000

    Change pyappcache dep into a note

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click",
         "platformdirs",
-        "pyappcache>0.9.1",
+        "pyappcache",  # ">0.9.1",
         "requests",
         "toml",
         "importlib_resources; python_version<'3.9'",
