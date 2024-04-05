e9bf09bcae2ec5f6455128fc76bcac6836dd136e
Cal Paterson <cal@calpaterson.com>
2024-04-05 14:17:38 +0300

    Upgrade to the new release of pyappcache

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         "click",
         "platformdirs",
-        "pyappcache",  # ">0.9.1",
+        "pyappcache>=0.10.0",
         "requests",
         "toml",
         "importlib_resources; python_version<'3.9'",
