fd0564d0aed4b042ed5a102d4afd2e6847658fd4
Cal Paterson <cal@calpaterson.com>
2024-06-11 14:21:45 +0300

    Sort out Github Actions, attempt 1

---
+++ tests/init-schemas.sql
@@ -0,0 +1,5 @@
+-- FIXME: vendored from csvbase.  should be a manifest file
+-- This SQL script creates the two schemas that are used for it's own tables
+-- and the tables of users respectively
+CREATE SCHEMA IF NOT EXISTS metadata;
+CREATE SCHEMA IF NOT EXISTS userdata;
